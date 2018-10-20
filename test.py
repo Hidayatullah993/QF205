f1710fe0648e04bf0bf7e8ec52ebffb76450e878
Hidayatullah <hidaya.2016@sis.smu.edu.sg>
2018-10-20 15:19:23 +0800

    Testing GUI creation

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("hello world")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class Test(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        self.resize(250,150)
+        self.move(300,300)
+        self.setWindowTitle("Test")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    test = Test()
+    test.show()
+    sys.exit(app.exec_())
